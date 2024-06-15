37f9f52cf7e1b81c4adf7a275650b51b60e8903f
Hasse <Dima8819@yandex.ru>
2024-06-15 06:44:43 +0200

    - Added images of chess pieces to the project. - The chess set is now created directly in the graphic scene. - Added numbering of cells (a-h and 1-8) along the edges of the chessboard. - Used QPainter to draw numbering on the widget.

--- chessboard.cpp
+++ chessboard.cpp
@@ -1,27 +1,76 @@
 #include "chessboard.h"
 
+
 ChessBoard::ChessBoard(QWidget *parent)
-    : QWidget{parent}
+    : QGraphicsView{parent}
 {
     setFixedSize(760, 760);
+    this->m_square_Size = (this->width() - 60) / 8;
+    this->m_indentation = 30;
+    this->scene = new QGraphicsScene(this);
+    this->scene->setSceneRect(0, 0, this->width(), this->height());
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    this->Paint_Board();
+    this->setScene(this->scene);
 }
 
-void ChessBoard::paintEvent(QPaintEvent *p)
+ChessBoard::~ChessBoard()
 {
-    QPainter painter;
-    int squareSize = width() / 8;
-    painter.begin(this);
-    for (int i = 0; i < 8; i++)
+    for(int i = 0; i < this->m_vector_square.size(); i++)
     {
-        for (int j = 0; j < 8; j++)
+        delete m_vector_square[i];
+    }
+
+    for(int i = 0; i < this->m_vector_board_numbering.size(); i++)
+    {
+        delete m_vector_board_numbering[i];
+    }
+}
+
+void ChessBoard::Paint_Board()
+{
+    // Рисование шахматной доски
+
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
         {
-            if((i + j) % 2 == 0)
-            {
-                painter.setBrush(Qt::white);
-            }
-            else painter.setBrush(Qt::black);
-            painter.drawRect(j * squareSize, i * squareSize, squareSize, squareSize);
+            QColor color = (i + j) % 2 == 0 ? QColor(222, 184, 135) : QColor(139, 69, 19);
+            QGraphicsRectItem *square = scene->addRect(i * m_square_Size + m_indentation, j * m_square_Size + m_indentation, m_square_Size, m_square_Size, QPen(Qt::NoPen), QBrush(color));
+            this->m_vector_square.push_back(square);
         }
     }
-    painter.end();
+
+
 }
+
+void ChessBoard::paintEvent(QPaintEvent *event)
+{
+    QGraphicsView::paintEvent(event);
+
+    QPainter painter(viewport());
+
+    QFont font;
+    font.setPointSize(14);
+    painter.setFont(font);
+
+
+    // Добавление букв (a-h) внизу и вверху доски
+    for(int i = 0; i < 8; i++)
+    {
+        QString book = QChar('a' + i);
+        painter.drawText(i * m_square_Size + m_indentation * 2 + 8, this->height() - m_indentation + 20, book);
+        painter.drawText(i * m_square_Size + m_indentation * 2 + 8, m_indentation - 10, book);
+    }
+
+    // Добавление цифр (1-8) слева и справа от доски
+    for(int i = 0; i < 8; i++)
+    {
+        QString number = QString::number(8 - i);
+        painter.drawText(m_indentation - 20, i * m_square_Size + m_indentation * 2 + 18, number);
+        painter.drawText(this->width() - m_indentation + 5, i * m_square_Size + m_indentation * 2 + 18, number);
+    }
+}
+
+
--- chessboard.h
+++ chessboard.h
@@ -1,17 +1,28 @@
 #ifndef CHESSBOARD_H
 #define CHESSBOARD_H
 
-#include <QWidget>
 #include <QPainter>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+#include <QVector>
 
-class ChessBoard : public QWidget
+class ChessBoard : public QGraphicsView
 {
     Q_OBJECT
 public:
     explicit ChessBoard(QWidget *parent = nullptr);
-
+    ~ChessBoard();
+private:
+    QGraphicsScene *scene;
+    QVector<QGraphicsRectItem*> m_vector_square;
+    QVector<QGraphicsTextItem*> m_vector_board_numbering;
+    int m_square_Size;
+    int m_indentation;
+    void Paint_Board();
 protected:
-    void paintEvent(QPaintEvent* p) override;
+     void paintEvent(QPaintEvent *event) override;
 
 signals:
 };
